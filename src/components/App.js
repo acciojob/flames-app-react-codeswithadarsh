c0c8f130e3c90e3e8b57309d0573e72586cd9fb5
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-12-17 03:37:34 +0000

    app

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,95 @@
-import React, { Component, useState, useRef } from "react";
-import "../styles/App.css";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+// class App extends Component {
+//     render() {
+
+//         return(
+//             <div id="main">
+//               <FlamesApp/>
+//             </div>
+//         )
+//     }
+// }
 
 const App = () => {
-  const first = useRef(null);
-  const second = useRef(null);
-  const [status, setstatus] = useState("");
-  const handleStatus = () => {
-    let firstname = first.current.value;
-    let secondname = second.current.value;
-    let totallength = (firstname + secondname).length;
-    let similar = 0;
-    let different = 0;
-
-    for (let i = 0; i < firstname.length; i++) {
-      if (secondname[i] === firstname[i]) {
-        similar++;
-      } else {
-        different++;
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [relationshipStatus, setRelationshipStatus] = useState('');
+    
+    const calculateRelationship = () => {
+      if (!input1 || !input2) {
+        setRelationshipStatus('Please Enter valid input');
+        return;
       }
-    }
-    const output = (totallength - similar) % 6;
-    let result = "";
-    switch (output) {
-      case 0:
-        result = "Siblings";
-        break;
-      case 1:
-        result = "Friends";
-        break;
-      case 2:
-        result = "Love";
-        break;
-      case 3:
-        result = "Affection";
-        break;
-      case 4:
-        result = "Marriage";
-        break;
-      case 5:
-        result = "Enemy";
-        break;
-      default:
-        result = "";
-    }
-    setstatus(result);
+  
+      // Remove common characters
+      let remaining1 = input1;
+      let remaining2 = input2;
+  
+      for (let char of input1) {
+        if (remaining2.includes(char)) {
+          remaining1 = remaining1.replace(char, '');
+          remaining2 = remaining2.replace(char, '');
+        }
+      }
+  
+      const totalLength = (remaining1.length + remaining2.length) % 6;
+  
+      switch (totalLength) {
+        case 1:
+          setRelationshipStatus('Friends');
+          break;
+        case 2:
+          setRelationshipStatus('Love');
+          break;
+        case 3:
+          setRelationshipStatus('Affection');
+          break;
+        case 4:
+          setRelationshipStatus('Marriage');
+          break;
+        case 5:
+          setRelationshipStatus('Enemy');
+          break;
+        case 0:
+          setRelationshipStatus('Siblings');
+          break;
+        default:
+          setRelationshipStatus('');
+      }
+    };
+  
+    const clearInputs = () => {
+      setInput1('');
+      setInput2('');
+      setRelationshipStatus('');
+    };
+  
+    return (
+      <div>
+        <input
+          type="text"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+          data-testid="input1"
+        />
+        <input
+          type="text"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+          data-testid="input2"
+        />
+        <button onClick={calculateRelationship} data-testid="calculate_relationship">
+          Calculate Relationship Future
+        </button>
+        <button onClick={clearInputs} data-testid="clear">
+          Clear
+        </button>
+        <h3 data-testid="answer">{relationshipStatus}</h3>
+      </div>
+    );
   };
-  return (
-    <div id="main">
-      {/* Do not remove the main div */}
-      <input type="text" ref={first} id="fname" data-testid="input1" />
-      <input type="text" ref={second} id="lname" data-testid="input2" />
-      <button
-        type="button"
-        data-testid="calculate_relationship"
-        onClick={handleStatus}
-      >
-        Calculate Relationship Future
-      </button>
-      <button
-        type="button"
-        data-testid="clear"
-        onClick={() => {
-          setstatus("");
-        }}
-      >
-        Clear inputs and relationship status
-      </button>
-      <h3 data-testid="answer">{status}</h3>
-    </div>
-  );
-};
+
 
 export default App;
